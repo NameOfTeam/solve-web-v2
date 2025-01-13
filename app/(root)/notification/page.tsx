e6d6cb27f1ca28fe7409ffbc354b378418495f00
dksgusdn61 <dksgusdn61@gmail.com>
2025-01-13 16:19:05 +0900

    feat: notification page

---
+++ app/(root)/notification/page.tsx
@@ -0,0 +1,89 @@
+import Banner from "@/components/ui/Banner";
+import React from "react";
+
+const Notification = () => {
+  return (
+    <div className="w-full">
+      <Banner title="알림" description="알림을 확인해 보세요." />
+      <div className="flex flex-col gap-6 px-52 py-6">
+        <div className="flex flex-col gap-3">
+          <p className="border-b border-bg-border pb-2 text-lg text-main-container">
+            2025.1.9
+          </p>
+          <div className="flex flex-col gap-3">
+            <div>
+              <p className="font-400 text-lg text-main-container">
+                개발자 커뮤니티 오픈
+              </p>
+              <p className="font-400 text-sm text-bg-border">08:15 PM</p>
+            </div>
+            <div>
+              <p className="font-400 text-lg text-main-container">
+                개발자 커뮤니티 오픈
+              </p>
+              <p className="font-400 text-sm text-bg-border">08:15 PM</p>
+            </div>
+            <div>
+              <p className="font-400 text-lg text-main-container">
+                개발자 커뮤니티 오픈
+              </p>
+              <p className="font-400 text-sm text-bg-border">08:15 PM</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <p className="border-b border-bg-border pb-2 text-lg text-main-container">
+            2025.1.9
+          </p>
+          <div className="flex flex-col gap-3">
+            <div>
+              <p className="font-400 text-lg text-main-container">
+                개발자 커뮤니티 오픈
+              </p>
+              <p className="font-400 text-sm text-bg-border">08:15 PM</p>
+            </div>
+            <div>
+              <p className="font-400 text-lg text-main-container">
+                개발자 커뮤니티 오픈
+              </p>
+              <p className="font-400 text-sm text-bg-border">08:15 PM</p>
+            </div>
+            <div>
+              <p className="font-400 text-lg text-main-container">
+                개발자 커뮤니티 오픈
+              </p>
+              <p className="font-400 text-sm text-bg-border">08:15 PM</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <p className="border-b border-bg-border pb-2 text-lg text-main-container">
+            2025.1.9
+          </p>
+          <div className="flex flex-col gap-3">
+            <div>
+              <p className="font-400 text-lg text-main-container">
+                개발자 커뮤니티 오픈
+              </p>
+              <p className="font-400 text-sm text-bg-border">08:15 PM</p>
+            </div>
+            <div>
+              <p className="font-400 text-lg text-main-container">
+                개발자 커뮤니티 오픈
+              </p>
+              <p className="font-400 text-sm text-bg-border">08:15 PM</p>
+            </div>
+            <div>
+              <p className="font-400 text-lg text-main-container">
+                개발자 커뮤니티 오픈
+              </p>
+              <p className="font-400 text-sm text-bg-border">08:15 PM</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Notification;
