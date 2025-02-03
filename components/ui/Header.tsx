394cc3708e4e98faa12f6f392d796204f6f09fc4
Cartridge0923 <pjmin0923@gmail.com>
2025-02-03 13:58:29 +0900

    feat: header responsivity

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -19,21 +19,30 @@ const Header = () => {
 
   return (
     <div className="w-full flex justify-center bg-container border-b border-bg-border ">
-      <div className="w-[72%] h-[72px] flex items-center gap-x-10 relative">
-        <Link href="/" className="cursor-pointer text-main-container">
-          <Logo width={116} height={52} />
-        </Link>
+      <div className="w-[72%] h-[72px] flex items-center justify-between relative text-main-container">
 
-        <div className="gap-x-7 h-full flex items-center text-base text-main-container header relative max-md:hidden">
-          <Link href="/problems">문제</Link>
-          <Link href="/workbooks">문제집</Link>
-          <Link href="/contests">대회</Link>
-          <Link href="/boards">게시판</Link>
-          <Link href="/store">상점</Link>
+        <div className=" flex items-center text-base h-full text-main-container header relative whitespace-nowrap z-[999]">
+
+          <svg className="lg:hidden stroke-main-container w-[36px] h-full" viewBox="0 0 24 24">
+            <path d="M4 6H20M4 12H20M4 18H20" strokeWidth="2px" stroke-linecap="round" stroke-linejoin="round" width="36px" height="36px"/>
+          </svg>
+
+          <div className="gap-x-7 flex max-lg:hidden ">
+            <Link href="/problems">문제</Link>
+            <Link href="/workbooks">문제집</Link>
+            <Link href="/contests">대회</Link>
+            <Link href="/boards">게시판</Link>
+            <Link href="/store">상점</Link>
+          </div>
+
           <HeaderExtension />
         </div>
 
-        <div className="flex-1" />
+        <div className="absolute flex justify-center w-full">
+          <Link href="/" className="cursor-pointer text-main-container">
+            <Logo width={116} height={52} />
+          </Link>
+        </div>
 
         {user?.id ? (
           <Link
@@ -49,9 +58,10 @@ const Header = () => {
             />
           </Link>
         ) : (
-          <Link href="/login" className="font-bold text-main-container">
+          <Link href="/login" className="font-bold text-main-container whitespace-nowrap">
             로그인
           </Link>
+
         )}
       </div>
     </div>
